6d473dc4d7099525e458f3130fece1ea4a2bd98e
kaylafinkelstein <k.finkelstein4000@gmail.com>
2022-07-06 12:28:29 -0400

    Weekly Challenges

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,83 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            bool containsWord = false;
+            if(string.Equals(words, null) || words.Contains(null))
+            {
+                return false;
+            }
+            if(ignoreCase == true)
+            {
+                word = word.ToLower();
+                List<string> lc = words.Select(x => x.ToLower()).ToList();
+                containsWord = lc.Contains(word);
+            }
+            if(ignoreCase == false)
+            {
+                containsWord = words.Contains(word);
+            }
+            return containsWord;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+        
+            if(num > 1)
+            {
+                return Enumerable.Range(1, num).Where(x => num % x == 0).SequenceEqual(new[] { 1, num });
+            }
+            return false;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            return str.IndexOf(str.Distinct().Reverse().Where(x =>str.Where(y => y.Equals(x)).Count() == 1).FirstOrDefault());
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                int CurrentCount = 1; 
+                for(int j = i + 1; j < numbers.Length; j++)
+                {
+                    if(numbers[i] != numbers[j])
+                    {
+                        break;
+                    }
+                    CurrentCount++;
+                }
+                if(CurrentCount > count)
+                {
+                    count = CurrentCount;
+                }
+            }
+            return count;
+
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            List<double> result = new List<double>();
+            double[] nulls = new double[0];
+            if(elements == null)
+            {
+                return nulls;
+            }
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] % n == 0)
+                {
+                    result.Add(elements[i]);
+                }
+                if (n < 0 || n > elements.Count)
+                {
+                    result.Clear();
+                }
+            }
+            double[] answer = result.ToArray();
+            return answer;
         }
     }
 }
